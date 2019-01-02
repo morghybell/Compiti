33687a7ea77f8e906bc275bbb09c49287bc66f82
Morgana Bellacci <moghymorghy@gmail.com>
2019-01-02 16:23:27 +0100

    Inseriti commenti per le righe 14 e 15

---
+++ 3BI_Bellacci_Compiti/Compiti_Vacanze/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiti_Vacanze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.MaxValue;
+            Console.WriteLine(a);  //Stampa il valore massimo positivo rappresentabile in 32 bit
+            Console.WriteLine(a + 1);  //Stampa il valore minimo negativo rappresentabile in 32 bit
+            int b = -128;
+            int c = -b;
+            Console.WriteLine(Convert.ToString(b, 2));
+            Console.WriteLine(Convert.ToString(c, 2));
+            int value = 31;
+            int somma = value;
+            value = ~value + 1; //NOT
+            //value = value + 1;
+            somma = somma + value;
+            Console.WriteLine($"{value} e {somma}");
+            long number = 55555;
+            Console.WriteLine(Convert.ToString(number, 2));
+            short number1 = (short)number;
+            Console.WriteLine(number1);
+
+            Console.ReadLine();
+        }
+    }
+}
